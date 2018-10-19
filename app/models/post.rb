5106f64b3006d36503f288efafb6b1a3f62f3e28
serpentz <absalomcherinet@gmail.com>
2018-10-19 12:15:07 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+validates :title, presence: true
+  #category is either "Fiction" or "Non-Fiction"
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  #content is at least 100 characters long
+  validates :content, length: {minimum: 100}#content is at least 100 characters long
 end
